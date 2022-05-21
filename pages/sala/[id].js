a1a3e04ad41a5f059e6bbb665a323660f756bc50
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-05-21 00:12:42 -0300

    tentamo familia

--- pages/sala/[id].js
+++ pages/sala/[id].js
@@ -4,6 +4,7 @@ import { useRouter } from "next/dist/client/router";
 import axios from "axios";
 
 export default function Sala() {
+	const [input, setInput] = useState("");
 	const [dados, setDados] = useState({ perguntas: [] });
 	const { query } = useRouter();
 	useEffect(() => {
@@ -14,14 +15,51 @@ export default function Sala() {
 			setDados({ ...response.data });
 		});
 	}, []);
-	console.log(dados);
+	console.log(dados.perguntas);
 	return (
 		<>
 			<Header>{dados.nome_sala}</Header>
 			<Main>
-				{dados.perguntas.map((element) => {
-					return <Card>{element}</Card>;
+				{dados.perguntas.map((element, i) => {
+					return (
+						<Card key={i}>
+							<div className='titulo'>{element.titulo}</div>
+							<div
+								className={`resposta ${
+									element.respostas.respondido ? "" : "hidden"
+								}`}>
+								{element.respostas.mensagem}
+							</div>
+							{element.respostas.respondido ? (
+								<></>
+							) : (
+								<form
+									onSubmit={(e) => {
+										e.preventDefault();
+									}}>
+									<input type='text' />
+									<button>Enviar Resposta</button>
+								</form>
+							)}
+						</Card>
+					);
 				})}
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						const promise = axios.post("https://hackathoncamelgrup.herokuapp.com/post", {
+							id: query.id,
+							titulo: input,
+						})
+					}}>
+					<input
+						type='text'
+						onChange={(e) => {
+							setInput(e.target.value);
+						}}
+					/>
+					<button>Fazer Pergunta</button>
+				</form>
 			</Main>
 		</>
 	);
@@ -45,8 +83,42 @@ const Main = styled.div`
 	width: 100vw;
 `;
 const Card = styled.div`
-	background-color: #00fcfc;
-	width: 80%;
-	height: 350px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 70%;
+	height: 250px;
 	margin-top: 50px;
+	border-radius: 25px;
+	box-shadow: 10px 10px 12px 1px rgba(0, 0, 0, 0.75);
+	.titulo {
+		margin-top: 20px;
+		font-size: 24px;
+		font-weight: 600;
+		font-family: "Roboto Flex";
+	}
+	.resposta {
+		margin-top: 70px;
+		font-size: 24px;
+		font-weight: 400;
+		font-family: "Roboto Flex";
+	}
+	form {
+		display: flex;
+		flex-direction: column;
+		margin-top: 30px;
+	}
+	input {
+		height: 50px;
+		margin-bottom: 15px;
+		font-size: 24px;
+		font-weight: 400;
+		font-family: "Roboto Flex";
+	}
+	button {
+		height: 30px;
+	}
+	.hidden {
+		display: none;
+	}
 `;
